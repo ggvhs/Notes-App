3a4e8b961bde73e627e40846c0f0ce3fbab8f712
gg_ <89791105+ggvhs@users.noreply.github.com>
2023-06-21 15:28:42 -0700

    commit

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,4 +1,10 @@
 const addBtn = document.getElementById('add');
+
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes){
+    notes.forEach(notes => addNewNote(note))
+}
 console.log(addBtn);
 
 
@@ -47,7 +53,32 @@ function addNewNote(text = ''){
    editBtn.addEventListener('click', () =>{
     main.classList.toggle('hidden')
     textArea.classList.toggle('hidden')
+   });
+
+   textArea.addEventListener('input', (e)=>{
+    const value = e.target.value;
+    console.log(value);
+    main.innerHTML = value;
+
+    updateLS();
    })
 
    document.body.appendChild(note);
+
+   
 }
+
+
+
+function update(){
+    const notesText = document.querySelectorAll('textarea');
+
+    const notes = [];
+
+    notesText.forEach(note => notes.push(note.value))
+
+    // console.log(notes, notesText)
+
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
